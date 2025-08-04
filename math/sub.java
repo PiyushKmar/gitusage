286cc24243e33c16637d5d55cf04b52b98fc377d
Piyush <piyushkumar200617@gmail.com>
2025-08-04 10:49:33 +0530

    [Piyush Kumar] added a folder as math

---
+++ math/sub.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=5;
+	int b=10;
+	if(a>b){
+		System.out.pritnln(a-b);}
+	else{
+		
+	System.out.println(b-a);}
+	System.out.println("IT is program of subtraction");
+    }
+}
